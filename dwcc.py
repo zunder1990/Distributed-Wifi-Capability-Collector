65cba8551207285fd756463ca7d91c02d6173d45
Zach Underwood <zunder1990@gmail.com>
2018-01-05 15:46:27 -0500

    working on added support for more than 1 sniffer interfaces

--- dwcc.py
+++ dwcc.py
@@ -13,19 +13,44 @@ import subprocess
 import os.path
 import pysftp
 
-#one day there will be support for more than one interface
-interface = 'wlan1mon'
-monitor_enable  = 'ifconfig wlan1 down; iw dev wlan1 interface add wlan1mon type monitor; ifconfig wlan1mon down; iw dev wlan1mon set type monitor; ifconfig wlan1mon up'
-monitor_disable = 'iw dev wlan1mon del; ifconfig wlan1 up'
-change_channel  = 'iw dev wlan1mon set channel %s'
+#1 is enabled, 0 is disabled
+interface1enable = 1
+interface1 = 'wlan1mon'
+monitor_enable1  = 'ifconfig wlan1 down; iw dev wlan1 interface add wlan1mon type monitor; ifconfig wlan1mon down; iw dev wlan1mon set type monitor; ifconfig wlan1mon up'
+monitor_disable1 = 'iw dev wlan1mon del; ifconfig wlan1 up'
+change_channel1  = 'iw dev wlan1mon set channel %s'
+channels1 = [6, 48, 1, 11, 36, 40] #use the linux command "iwlist channel" to get a list of every channel your devices supports)
 
-#one day this will be changed to support 5ghz and 2ghz with two lists. Right now this works with a single daul band device
-channels = [6, 48, 1, 11, 36, 40]
+#At this more than interface has not been tested
+#1 is enabled, 0 is disabled
+interface2enable = 0
+interface2 = 'wlan1mon'
+monitor_enable2  = 'ifconfig wlan1 down; iw dev wlan1 interface add wlan1mon type monitor; ifconfig wlan1mon down; iw dev wlan1mon set type monitor; ifconfig wlan1mon up'
+monitor_disable2 = 'iw dev wlan1mon del; ifconfig wlan1 up'
+change_channel2  = 'iw dev wlan1mon set channel %s'
+channels2 = [6, 48, 1, 11, 36, 40] #use the linux command "iwlist channel" to get a list of every channel your devices supports)
+
+#At this more than interface has not been tested
+#1 is enabled, 0 is disabled
+interface3enable = 0
+interface3 = 'wlan1mon'
+monitor_enable3  = 'ifconfig wlan1 down; iw dev wlan1 interface add wlan1mon type monitor; ifconfig wlan1mon down; iw dev wlan1mon set type monitor; ifconfig wlan1mon up'
+monitor_disable3 = 'iw dev wlan1mon del; ifconfig wlan1 up'
+change_channel3  = 'iw dev wlan1mon set channel %s'
+channels3 = [6, 48, 1, 11, 36, 40] #use the linux command "iwlist channel" to get a list of every channel your devices supports)
+
+#At this more than interface has not been tested
+#1 is enabled, 0 is disabled
+interface4enable = 0
+interface4 = 'wlan1mon'
+monitor_enable4  = 'ifconfig wlan1 down; iw dev wlan1 interface add wlan1mon type monitor; ifconfig wlan1mon down; iw dev wlan1mon set type monitor; ifconfig wlan1mon up'
+monitor_disable4 = 'iw dev wlan1mon del; ifconfig wlan1 up'
+change_channel4  = 'iw dev wlan1mon set channel %s'
+channels = [6, 48, 1, 11, 36, 40] #use the linux command "iwlist channel" to get a list of every channel your devices supports)
 
 #info for sftp server
 sshhost = '192.168.1.144'  #can be hostname or ip
 sshuser = 'zach'           #make sure that key auth is working
-
 
 hostname = socket.gethostname()
 
@@ -35,31 +60,65 @@ incomingpath = '/data/incoming/' #This is the path where new pcaps will be place
 #This is the main function
 def start():
 	logging.basicConfig(filename='dwcc.log', format='%(levelname)s:%(message)s', level=logging.INFO)
-	os.system(monitor_enable)
-	stop_rotating = rotator(channels, change_channel)
+	if interface1enable = 1
+		os.system(monitor_enable1)
+	if interface2enable = 1
+		os.system(monitor_enable1)
+	if interface3enable = 1
+		os.system(monitor_enable1)
+	if interface4enable = 1
+		os.system(monitor_enable1)
+	stop_rotating = rotator()
 	stop_uploading = uploader()
 	try:sniffer(interface)
 	except KeyboardInterrupt: sys.exit()
 	finally:
 		stop_rotating.set()
 		stop_uploading.set()
-		os.system(monitor_disable)
+		if interface1enable = 1
+			os.system(monitor_disable1)
+		if interface2enable = 1
+			os.system(monitor_disable2)
+		if interface3enable = 1
+			os.system(monitor_disable3)
+		if interface4enable = 1
+			os.system(monitor_disable4
 #This will change the channels every 1 sec to scan all in the range. One day there will be support for more than one rotator to support 2.4ghz and 5ghz.		
-def rotator(channels, change_channel):
+def rotator():
     def rotate(stop):
         while not stop.is_set():
             try:
-                channel = str(random.choice(channels))
-                logging.info('Changing to channel ' + channel)
-                os.system(change_channel % channel)
-                time.sleep(1) # seconds
+                if interface1enable = 1 #This loop is for interface 1
+					channel1 = str(random.choice(channels1))
+					logging.info('Changing to channel ' + channel1)
+					os.system(change_channel1 % channel1)
+				if interface2enable = 1 #This loop is for interface 2
+					channel2 = str(random.choice(channels2))
+					logging.info('Changing to channel ' + channel2)
+					os.system(change_channel2 % channel2)
+				if interface3enable = 1 #This loop is for interface 3
+					channel3 = str(random.choice(channels3))
+					logging.info('Changing to channel ' + channel3)
+					os.system(change_channel3 % channel3)
+				if interface4enable = 1 #This loop is for interface 4
+					channel4 = str(random.choice(channels4))
+					logging.info('Changing to channel ' + channel4)
+					os.system(change_channel4 % channel4)
+				time.sleep(1) # seconds
             except KeyboardInterrupt: pass
     stop = multiprocessing.Event()
     multiprocessing.Process(target=rotate, args=[stop]).start()
     return stop
 #this is the caputre fuction, It will only caputre the mgt frames.
 def sniffer(interface):
-	subprocess.call('tcpdump -i wlan1mon -G 600 --packet-buffered -W 144 -e -s 512 type mgt -w /data/incoming/trace-%Y-%m-%d_%H.%M.%S.pcap', shell=True)
+	if interface1enable = 1
+		subprocess.call('tcpdump -i wlan1mon -G 600 --packet-buffered -W 144 -e -s 512 type mgt -w /data/incoming/trace-%Y-%m-%d_%H.%M.%S.pcap', shell=True)
+	if interface2enable = 1
+		print "interface2 sniffer would have ran"
+	if interface3enable = 1
+		print "interface3 sniffer would have ran"
+	if interface4enable = 1
+		print "interface4 sniffer would have ran"
 #the above will rotate the pcap every 10 mins and keeps 24 hours worth
 def uploader():
     def upload(stop):
