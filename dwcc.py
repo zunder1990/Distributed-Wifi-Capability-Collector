bc43edce10c4e3f779fa493b7e6f5c009f95e78e
Zach Underwood <zunder1990@gmail.com>
2018-01-05 16:00:12 -0500

    working on added support for more than 1 sniffer interfaces

--- dwcc.py
+++ dwcc.py
@@ -60,47 +60,48 @@ incomingpath = '/data/incoming/' #This is the path where new pcaps will be place
 #This is the main function
 def start():
 	logging.basicConfig(filename='dwcc.log', format='%(levelname)s:%(message)s', level=logging.INFO)
-	if interface1enable = 1
+	if interface1enable == '1':
 		os.system(monitor_enable1)
-	if interface2enable = 1
+	if interface2enable == '1':
 		os.system(monitor_enable1)
-	if interface3enable = 1
+	if interface3enable == '1':
 		os.system(monitor_enable1)
-	if interface4enable = 1
+	if interface4enable == '1':
 		os.system(monitor_enable1)
 	stop_rotating = rotator()
 	stop_uploading = uploader()
 	try:sniffer(interface)
 	except KeyboardInterrupt: sys.exit()
 	finally:
+		print "Please wait for everything to stop"
 		stop_rotating.set()
 		stop_uploading.set()
-		if interface1enable = 1
+		if interface1enable == '1':
 			os.system(monitor_disable1)
-		if interface2enable = 1
+		if interface2enable == '1':
 			os.system(monitor_disable2)
-		if interface3enable = 1
+		if interface3enable == '1':
 			os.system(monitor_disable3)
-		if interface4enable = 1
+		if interface4enable == '1':
 			os.system(monitor_disable4
 #This will change the channels every 1 sec to scan all in the range. One day there will be support for more than one rotator to support 2.4ghz and 5ghz.		
 def rotator():
     def rotate(stop):
         while not stop.is_set():
             try:
-                if interface1enable = 1 #This loop is for interface 1
+                if interface1enable == '1': #This loop is for interface 1
 					channel1 = str(random.choice(channels1))
 					logging.info('Changing to channel ' + channel1)
 					os.system(change_channel1 % channel1)
-				if interface2enable = 1 #This loop is for interface 2
+				if interface2enable == '1': #This loop is for interface 2
 					channel2 = str(random.choice(channels2))
 					logging.info('Changing to channel ' + channel2)
 					os.system(change_channel2 % channel2)
-				if interface3enable = 1 #This loop is for interface 3
+				if interface3enable == '1': #This loop is for interface 3
 					channel3 = str(random.choice(channels3))
 					logging.info('Changing to channel ' + channel3)
 					os.system(change_channel3 % channel3)
-				if interface4enable = 1 #This loop is for interface 4
+				if interface4enable == '1': #This loop is for interface 4
 					channel4 = str(random.choice(channels4))
 					logging.info('Changing to channel ' + channel4)
 					os.system(change_channel4 % channel4)
@@ -111,13 +112,13 @@ def rotator():
     return stop
 #this is the caputre fuction, It will only caputre the mgt frames.
 def sniffer(interface):
-	if interface1enable = 1
+	if interface1enable == '1':
 		subprocess.call('tcpdump -i wlan1mon -G 600 --packet-buffered -W 144 -e -s 512 type mgt -w /data/incoming/trace-%Y-%m-%d_%H.%M.%S.pcap', shell=True)
-	if interface2enable = 1
+	if interface2enable == '1':
 		print "interface2 sniffer would have ran"
-	if interface3enable = 1
+	if interface3enable == '1':
 		print "interface3 sniffer would have ran"
-	if interface4enable = 1
+	if interface4enable == '1':
 		print "interface4 sniffer would have ran"
 #the above will rotate the pcap every 10 mins and keeps 24 hours worth
 def uploader():
