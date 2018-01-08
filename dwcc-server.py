5a1e7a89eb9966b32520be74b4201d4ceaf4d740
Zach Underwood <zunder1990@gmail.com>
2018-01-07 21:14:12 -0500

    fixed the problem with mac address convetor and added support for grouping/counting vendor

--- dwcc-server.py
+++ dwcc-server.py
@@ -75,6 +75,8 @@ def charting():
 	n80211usupport=cursor.fetchone()[0]
 	cursor.execute('SELECT COUNT(*) FROM dwccincoming WHERE wlanmgtextcapb32 = 1;')
 	qosmapsupport=cursor.fetchone()[0]
+	cursor.execute('SELECT wlansaconverted, count(wlansaconverted) FROM dwccincoming GROUP BY wlansaconverted ORDER BY count(wlansaconverted) DESC;')
+	devicemaker=cursor.fetchall()
 	print "Total number of clients found to support BSS Transition aka 802.11r aka FT = ", b19supportcount
 	print "Total number of clients found to support 80mhz channel in 5ghz = ", n80mhzsupportcount
 	print "Total number of clients found to support 160mhz channel in 5ghz = ", n160mhzsupportcount
@@ -83,7 +85,7 @@ def charting():
 	print "Total number of clients that support 802.11w= ", n80211wsupport
 	print "Total number of clients that support interworking this is reated to 802.11u= ", n80211usupport
 	print "Total number of clients that support QOS map= ", qosmapsupport
-
+	print devicemaker
 
 def dbconverter():
 	cursor.execute("UPDATE dwccincoming SET wlanmgtvhtcapabilitiessoundingdimensions = '1' WHERE wlanmgtvhtcapabilitiessoundingdimensions  = '0x00000001';")
@@ -129,14 +131,18 @@ def tsharker():
 def macaddressconverter():
 	p = manuf.MacParser(update=True)
 	cursor.execute("SELECT wlansa from dwccincoming WHERE wlansaconverted IS NULL OR wlansaconverted = '' limit 1;")
-	mactochange=cursor.fetchone()[0]
-	changedmac = p.get_manuf(mactochange)
-	changedmacstr = str(changedmac)
-	mactochangestr = str(mactochange)
-	cursor.execute("UPDATE dwccincoming SET wlansaconverted = "+ `changedmacstr` +" WHERE wlansa  = "+ `mactochangestr` +"   ;")
-	print changedmac
-	print mactochange
-	conn.commit()
+	mactochange=cursor.fetchone()
+	if mactochange is None:
+		print "all MAC matched to vendors"
+	else:
+		changedmac = p.get_manuf(mactochange)
+		changedmacstr = str(changedmac)
+		mactochangestr = str(mactochange)
+		cursor.execute("UPDATE dwccincoming SET wlansaconverted = "+ `changedmacstr` +" WHERE wlansa  = "+ `mactochangestr` +"   ;")
+#		print changedmac
+#		print mactochange
+		conn.commit()
+		macaddressconverter()
 						
 def dbupdater():
 	
