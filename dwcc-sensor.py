9b6e908a8d4a24388b7b67281d5bf12eb1609cd6
Zach Underwood <zunder1990@gmail.com>
2018-02-23 02:44:32 +0000

    added logging support

--- dwcc-sensor.py
+++ dwcc-sensor.py
@@ -65,20 +65,24 @@ incomingpath = '/data/incoming/' #This is the path where new pcaps will be place
 
 #This is the main function
 def start():
+	try:
+    		os.remove("/root/Distributed-Wifi-Capability-Collector/dwcc.log")
+	except OSError:
+    		pass
 	subprocess.call('iw reg set US', shell=True)
-	logging.basicConfig(filename='dwcc.log', format='%(levelname)s:%(message)s', level=logging.INFO)
+	logging.basicConfig(filename='/root/Distributed-Wifi-Capability-Collector/dwcc.log', format='%(levelname)s:%(message)s', level=logging.INFO)
 	if iamapi == '1':
 		gpinsetup()
 		stop_networkchecker = networkchecker()
 	if interface1enable == '1':
 		os.system(monitor_enable1)
-		print "starting wlan1"
+		logging.info("starting wlan1")
 	if interface2enable == '1':
 		os.system(monitor_enable2)
-		print "starting wlan2"
+		logging.info("starting wlan2")
 	if interface3enable == '1':
 		os.system(monitor_enable3)
-		print "starting wlan3"
+		logging.info("starting wlan3")
 	stop_rotating = rotator()
 	stop_uploading = uploader()
 	try:sniffer()
@@ -110,14 +114,14 @@ def networkchecker():
 			try:
 				response = os.system("ping -c 2 google.com")
 				if response == 0:
-					print('network is up!')
-					print "port 13 LED on"
+					logging.info('network is up!')
+					logging.info ("port 13 LED on")
 					GPIO.output(13,GPIO.HIGH)
 				else:
-					print('network is down!')
-					print "port 13 LED off"
+					logging.info('network is down!')
+					logging.info ("port 13 LED off")
 					GPIO.output(13,GPIO.LOW)
-				print "retesting in 5 min"
+				logging.info ("retesting in 5 min")
 				time.sleep(300) # seconds
 			except KeyboardInterrupt: pass
 	stop = multiprocessing.Event()
@@ -130,34 +134,31 @@ def rotator():
 			try:
 				if interface1enable == '1': #This loop is for interface 1
 					channel1 = str(random.choice(channels1))
-					#logging.info('Changing to channel for interface1 ' + channel1)
-					print ('Changing to channel for interface1 ' + channel1)
+					logging.info('Changing to channel for interface1 ' + channel1)
 					os.system(change_channel1 % channel1)
 				if interface2enable == '1': #This loop is for interface 2
 					channel2 = str(random.choice(channels2))
-					#logging.info('Changing to channel for interface2 ' + channel2)
-					print ('Changing to channel for interface2 ' + channel2)
+					logging.info('Changing to channel for interface2 ' + channel2)
 					os.system(change_channel2 % channel2)
 				if interface3enable == '1': #This loop is for interface 3
 					channel3 = str(random.choice(channels3))
-					#logging.info('Changing to channel for interface3 ' + channel3)
-					print ('Changing to channel for interface3 ' + channel3)
+					logging.info('Changing to channel for interface3 ' + channel3)
 					os.system(change_channel3 % channel3)
-				time.sleep(2) # seconds
+				time.sleep(5) # seconds
 			except KeyboardInterrupt: pass
 	stop = multiprocessing.Event()
 	multiprocessing.Process(target=rotate, args=[stop]).start()
 	return stop
 #this is the caputre fuction, It will only caputre the mgt frames.
 def sniffer():
-	print "sniffer started"
+	logging.info("sniffer started")
 	if iamapi == '1':
-		print "port 6 led on"
+		logging.info("port 6 led on")
 		GPIO.output(6,GPIO.HIGH)
 	commands = [
-    'tcpdump -i '+ interface1 +' -G 600 --packet-buffered -W 144 -e -s 1024 type mgt or type ctl -w '+incomingpath +''+ hostname +'-'+ interface1 +'-%Y-%m-%d_%H.%M.%S.pcap;',
-    'tcpdump -i '+ interface2 +' -G 600 --packet-buffered -W 144 -e -s 1024 type mgt or type ctl -w '+incomingpath +''+ hostname +'-'+ interface2 +'-%Y-%m-%d_%H.%M.%S.pcap;',
-    'tcpdump -i '+ interface3 +' -G 600 --packet-buffered -W 144 -e -s 1024 type mgt or type ctl -w '+incomingpath +''+ hostname +'-'+ interface3 +'-%Y-%m-%d_%H.%M.%S.pcap;',
+    'tcpdump -i '+ interface1 +' -G 600 --packet-buffered -W 300 -e -s 1024 type mgt or type ctl -w '+incomingpath +''+ hostname +'-'+ interface1 +'-%Y-%m-%d_%H.%M.%S.pcap;',
+    'tcpdump -i '+ interface2 +' -G 600 --packet-buffered -W 300 -e -s 1024 type mgt or type ctl -w '+incomingpath +''+ hostname +'-'+ interface2 +'-%Y-%m-%d_%H.%M.%S.pcap;',
+    'tcpdump -i '+ interface3 +' -G 600 --packet-buffered -W 300 -e -s 1024 type mgt or type ctl -w '+incomingpath +''+ hostname +'-'+ interface3 +'-%Y-%m-%d_%H.%M.%S.pcap;',
 ]
 #
 # run in parallel
@@ -177,13 +178,13 @@ def uploader():
 							with pysftp.Connection(host=sshhost, username=sshuser, private_key='~/.ssh/id_rsa') as sftp:
 								with sftp.cd(incomingpath):
 									sftp.put(incomingpath +fname)
-							print "moved", fname 
-						except pysftp.SSHException: 
-							print "Unable to establish SSH connection will retry in 5 min"
+							logging.info("moved", fname) 
+						except pysftp.SSHException:
+							logging.info("Unable to establish SSH connection will retry in 5 min")
 							time.sleep(300) #seconds
 					os.remove(incomingpath +fname)
 				else:
-					print "no pcap found, will try again in 5 min"
+					logging.info("no pcap found, will try again in 5 min")
 					time.sleep(300) #seconds
 			except KeyboardInterrupt: pass
 	stop = multiprocessing.Event()
